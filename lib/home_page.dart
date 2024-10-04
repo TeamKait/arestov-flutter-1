db0229d46476e095e62fb25eac37809f065edcfd
Студенты Арестова И.С. <areis@teach.local>
2024-10-04 16:27:54 +0300

    Theme

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,7 +10,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage>{
   Locale _locale = Locale("ru", "RU");
-
+  final MaterialColor _MAIN_COLOR = Colors.teal;
   void _changeLanguage(Locale locale){
     setState(() {
       _locale = locale;
@@ -32,7 +32,37 @@ class _HomePageState extends State<HomePage>{
       ],
       theme: ThemeData(
         brightness: Brightness.dark,
-        primarySwatch: Colors.teal
+        primarySwatch: Colors.teal,
+        scaffoldBackgroundColor: Colors.black87,
+        textTheme: TextTheme(
+          headlineLarge: TextStyle(color: _MAIN_COLOR),
+          bodyLarge: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.grey),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(color: _MAIN_COLOR)
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide(color: _MAIN_COLOR)
+          ),
+          labelStyle: TextStyle(color: _MAIN_COLOR) ,
+          filled: true,
+          fillColor: Colors.grey[800],
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: _MAIN_COLOR[300],
+            foregroundColor: Colors.black,
+            elevation: 8,
+            textStyle: TextStyle(fontSize: 18),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12)
+            )
+          )
+        )
       ),
     );
   }
